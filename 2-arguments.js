e1c853aeb19f09125fbf50731e1bafb256bdac61
janet wambua <janet2wambua@gmail.com>
2025-07-13 20:43:06 +0300

    This is test 3

---
+++ 2-arguments.js
@@ -0,0 +1,16 @@
+/* Write a script that prints a message depending of the number of arguments passed:
+If no arguments are passed to the script, print “No argument”
+If only one argument is passed to the script, print “Argument found”
+Otherwise, print “Arguments found”
+You must use console.log(...) to print all output
+You are not allowed to use var */
+ 
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log("No argument");
+} else if (args === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+};
