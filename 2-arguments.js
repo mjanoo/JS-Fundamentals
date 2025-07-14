ebd156545473c6e23e6db1e22e331a5193cf8f29
janet wambua <janet2wambua@gmail.com>
2025-07-14 18:32:33 +0300

    this is test 2

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,11 +5,13 @@ Otherwise, print “Arguments found”
 You must use console.log(...) to print all output
 You are not allowed to use var */
  
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+const args = process.argv
+
+if (args.length <= 2) {
+  console.log("No argument");
+} else if (args.length === 3) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
 
