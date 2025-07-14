1c28ce5a682dce8eabd532118695885e3642bcf9
janet wambua <janet2wambua@gmail.com>
2025-07-14 09:51:23 +0300

    this is test 9

--- 9-add.js
+++ 9-add.js
@@ -7,20 +7,11 @@
     You must use a loop (while, for, etc.)
  */
 
-   const agrCount = process.argv;
-const firstArg = process.argv[2];
+   const square = Number(process.argv[2]);
 
-let num = Number(firstArg);
-
-if (!isNaN(num)) {
-    for (let i = 0; i < num; i++) {
-        let row = ""; // عملنا دا عشان نعرف ان روو دا سترينج 
-        for (let j = 0; j < num; j++) {
-        row += "X"; // عملنا دى عشان مش هنقدر نطبع كل عدداكس جمب بعض فى سطر واحد 
-        }
-        console.log(row); // فبنضطر نخلى مجموعة اكس مع بعض سطر واحد و نطبع السطر على بعضه
-    }
+if (isNaN(square)) {
+  console.log('Missing size');
 }
-else{
-    console.log("Missing size");
+for (let i = 0; i < square; i++) {
+  console.log('X'.repeat(square));
 }
